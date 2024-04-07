7cb863ffa59763de187d2340b9aa432d7ab836a5
lenovo <3108531642@qq.com>
2024-04-07 10:37:18 +0800

    导航栏框架完成一部分

--- app/src/main/java/com/example/power/HomeActivity.kt
+++ app/src/main/java/com/example/power/HomeActivity.kt
@@ -20,5 +20,7 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
---
+++ app/src/main/java/com/example/power/ui/monitoring/MonitoringFragment.kt
@@ -0,0 +1,19 @@
+package com.example.power.ui.monitoring
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MonitoringFragment:Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+}
